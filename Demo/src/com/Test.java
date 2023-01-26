0a29e719d93737b9ef51429de681ecd52d5bf464
naikneha12 <nehadnaik2017@gmail.com>
2023-01-26 18:23:17 +0530

    Student Details added
    Student data set and Test class updated

--- Demo/src/com/Test.java
+++ Demo/src/com/Test.java
@@ -1,10 +1,18 @@
 package com;
 
 public class Test {
-	
+
 	public static void main(String[] args) {
-		
+
 		System.out.println("Test class created");
+
+		Student student = new Student();
+		student.setId(12);
+		student.setName("ABCD");
+		student.setAddress("Pune");
+
+		System.out.println("Student Details: + student");
+
 	}
 
 }
